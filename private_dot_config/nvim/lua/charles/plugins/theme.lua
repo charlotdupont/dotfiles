6543f197833862b78be892b35f9ec48a4b216528
Charles Dupont <charlo.dupont@gmail.com>
2025-01-09 09:19:39 -0500

    added plugins

--- private_dot_config/nvim/lua/charles/plugins/theme.lua
+++ private_dot_config/nvim/lua/charles/plugins/theme.lua
@@ -3,7 +3,16 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-        require("catppuccin").setup()
+        require("catppuccin").setup({
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                telescope = true,
+                notify = true,
+                mini = true,
+            }
+        })
 
         vim.cmd("colorscheme catppuccin-mocha")
     end
